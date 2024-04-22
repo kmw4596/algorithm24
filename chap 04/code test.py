328443a5b737535e0bb47df4e10f60e2ad5a4509
kmw4596 <kmw4596@gmail.com>
2024-04-22 18:46:51 +0900

    4.11

--- chap 04/code test.py
+++ chap 04/code test.py
@@ -24,6 +24,7 @@ insertion_sort(test_list)
 print("Sorted list:")
 print_list(test_list)
 '''
+'''
 def binary_search(A, key, low, high):
     if low <= high:
         mid = (low + high) // 2
@@ -42,3 +43,41 @@ if result != -1:
     print(f"Element found at index {result}")
 else:
     print("Element not found")
+
+def slow_power(x, n):
+    result = 1.0
+    for i in range(n):
+        result = result * x
+    return result
+
+x = 2
+n = 3
+result = slow_power(x, n)
+print(f"{x}의 {n} 거듭제곱 값은 {result}입니다.")
+'''
+def quick_select(A, left, right, k):
+    pos = partition(A, left, right)
+    if pos + 1 == k:
+        return A[pos]
+    elif pos + 1 > k:
+        return quick_select(A, left, pos - 1, k)
+    else:
+        return quick_select(A, pos + 1, right, k)
+
+def partition(A, left, right):
+    pivot = A[right]
+    i = left - 1
+    for j in range(left, right):
+        if A[j] <= pivot:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[right] = A[right], A[i + 1]
+    return i + 1
+
+# 테스트 리스트
+test_list = [3, 2, 1, 5, 4]
+# k번째 작은 수
+k = 3
+# k번째 작은 수 찾기
+result = quick_select(test_list, 0, len(test_list) - 1, k)
+print(f"{k}번째 작은 수는 {result}입니다.")
