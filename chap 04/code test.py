469545cfd60176a023df92eff539dab22ea718a8
kmw4596 <kmw4596@gmail.com>
2024-04-22 18:16:51 +0900

    4.5

--- chap 04/code test.py
+++ chap 04/code test.py
@@ -1,3 +1,4 @@
+'''
 def insertion_sort(A):
     n = len(A)
     for i in range(1, n):
@@ -22,3 +23,22 @@ insertion_sort(test_list)
 
 print("Sorted list:")
 print_list(test_list)
+'''
+def binary_search(A, key, low, high):
+    if low <= high:
+        mid = (low + high) // 2
+        if key == A[mid]:
+            return mid
+        elif key < A[mid]:
+            return binary_search(A, key, low, mid - 1)
+        else:
+            return binary_search(A, key, mid + 1, high)
+    return -1
+
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+key = 5
+result = binary_search(test_list, key, 0, len(test_list) - 1)
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
